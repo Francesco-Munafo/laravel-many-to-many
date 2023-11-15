460f6296004f87d265a299321188cf34fc077e38
Francesco-Munafo <acquamarea1@gmail.com>
2023-11-15 12:37:21 +0100

    Add orderByDesc in projects.index view

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -20,7 +20,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::paginate(5);
+        $projects = Project::orderByDesc('id')->paginate(5);
 
 
         return view('admin.projects.index', compact('projects'));
